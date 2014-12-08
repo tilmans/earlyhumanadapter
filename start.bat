5d5272d5019f705d8c3ae477394a11b3eb066cce
tilmans <tilman@mailbox.org>
2014-12-08 17:41:02 +0100

    tweaks

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 start %~dp0vvvv_45beta33.3_x86\vvvv.exe /o %~dp0projection\root.v4p
-start /d "C:\Program Files\Windows Media Player" wmplayer.exe /play %~dp0audio\earlysound6.mp3
+start /d "C:\Program Files\Windows Media Player" wmplayer.exe /play %~dp0audio\sound.wav
 start /d "%~dp0pd\bin" pd.exe -open %~dp0audio\sliderin.pd -noprefs -font-face "Lucida Console"  -lib Gem -lib cyclone -lib zexy -lib creb -lib cxc -lib iemlib -lib list-abs -lib mapping -lib markex -lib maxlib -lib mjlib -lib motex -lib oscx -lib pddp -lib pdogg -lib pmpd -lib sigpack -lib smlib -lib unauthorized -lib pan -lib hcs -lib jmmmp -lib ext13 -lib ggee -lib iem_anything -lib ekext -lib flatgui -lib chaos -lib zexy/0x260x260x7e -lib zexy/0x2e -lib zexy/0x3c0x7e -lib zexy/0x3d0x3d0x7e -lib zexy/0x3e0x7e -lib zexy/0x7c0x7c0x7e
